35af979e8d1fad5c94ed46d2d5c79debc8c2f4d8
wattson <oitimexo@gmail.com>
2022-11-22 01:01:27 +0700

    Add web icon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,28 @@ export default function Document() {
     return (
         <Html>
             <Head>
+                <meta name="title" content="wattson" />
+                <meta name="description" content="Blog & Article" />
+                <meta property="og:type" content="blog" />
+                <meta property="og:url" content="https://wattson.me/" />
+                <meta property="og:title" content="wattson" />
+                <meta property="og:description" content="Blog & Article" />
+                <meta
+                    property="og:image"
+                    content=""
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="32x32"
+                    href="/favicon-32x32.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="16x16"
+                    href="/favicon-16x16.png"
+                />
                 <link rel="manifest" href="/site.webmanifest" />
                 <meta name="theme-color" content="ffffff" />
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
@@ -19,5 +41,5 @@ export default function Document() {
                 <NextScript />
             </body>
         </Html>
-    );
+    )
 }
