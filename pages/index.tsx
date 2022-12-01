f0de8a14be4271440b9b75ab4cbf1445c1818734
wattson <oitimexo@gmail.com>
2022-12-01 17:22:03 +0700

    resume reference change (3)

--- pages/index.tsx
+++ pages/index.tsx
@@ -274,7 +274,7 @@ export default function Home({ }: Props) {
               <a>{`it's me "wattson"`}</a>
             </div>
             <span className="button-resume">
-              <a href="https://drive.google.com/file/d/1mEaj1fmSHH-3He5wHfQLBF6ul_xhcm4W/view?usp=sharing" target="_blank">resume</a>
+              <a href="https://drive.google.com/file/d/1zAxa1gpqZC-VQcUqB8mxtlV3-TeBBdvl/view?usp=sharing" target="_blank">resume</a>
             </span>
             <span className="button">
               <a href="https://github.com/wattsonz" target="_blank">github</a>
